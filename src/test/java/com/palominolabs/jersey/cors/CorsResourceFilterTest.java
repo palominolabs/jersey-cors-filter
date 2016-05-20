105c296c48b355340d9329ce5ea41f129b3fc838
Drew Stephens <drew@dinomite.net>
2016-05-20 10:23:08 -0400

    All annotation tests

--- src/test/java/com/palominolabs/jersey/cors/CorsResourceFilterTest.java
+++ src/test/java/com/palominolabs/jersey/cors/CorsResourceFilterTest.java
@@ -20,6 +20,9 @@ import static org.junit.Assert.assertFalse;
 public class CorsResourceFilterTest extends JerseyTest {
     @Path("unannotated")
     public static class UnAnnotatedResource {
+        @OPTIONS
+        public void optionsWithCorsPreflightAnnotation() { }
+
         @GET
         public String getWithCorsAnnotation() {
             return "foo";
@@ -66,6 +69,53 @@ public class CorsResourceFilterTest extends JerseyTest {
         }
     }
 
+    @Path("class-annotated-overrides")
+    @Cors(allowCredentials = TRUE, allowOrigin = "http://foo.com", exposeHeaders = "x-foo")
+    @CorsPreflight(allowCredentials = TRUE, allowHeaders = "x-bar", allowMethods = "POST", maxAge = 12345,
+            allowOrigin = "http://bar.com")
+    public static class ClassAnnotatedWithOverridesResource {
+        @OPTIONS
+        public void optionsWithCorsPreflightAnnotation() { }
+
+        @GET
+        public String getWithCorsAnnotation() {
+            return "foo";
+        }
+    }
+
+    @Path("class-annotated-overrides-annotated-methods")
+    @Cors(allowCredentials = TRUE, allowOrigin = "http://foo.com", exposeHeaders = "x-foo")
+    @CorsPreflight(allowCredentials = TRUE, allowHeaders = "x-bar", allowMethods = "POST", maxAge = 12345,
+            allowOrigin = "http://bar.com")
+    public static class ClassAnnotatedWithOverridesAndAnnotateMethodsResource {
+        @OPTIONS
+        @CorsPreflight
+        public void optionsWithCorsPreflightAnnotation() { }
+
+        @GET
+        @Cors
+        public String getWithCorsAnnotation() {
+            return "foo";
+        }
+    }
+
+    @Path("class-annotated-overrides-annotated-methods-overrides")
+    @Cors(allowCredentials = TRUE, allowOrigin = "http://foo.com", exposeHeaders = "x-foo")
+    @CorsPreflight(allowCredentials = TRUE, allowHeaders = "x-bar", allowMethods = "POST", maxAge = 12345,
+            allowOrigin = "http://bar.com")
+    public static class ClassAnnotatedWithOverridesAndAnnotateMethodsWithOverridesResource {
+        @OPTIONS
+        @CorsPreflight(allowCredentials = TRUE, allowHeaders = "x-bar", allowMethods = "POST", maxAge = 12345,
+                allowOrigin = "http://bar.com")
+        public void optionsWithCorsPreflightAnnotation() { }
+
+        @GET
+        @Cors(allowCredentials = TRUE, allowOrigin = "http://foo.com", exposeHeaders = "x-foo")
+        public String getWithCorsAnnotation() {
+            return "foo";
+        }
+    }
+
     @Override
     protected Application configure() {
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
@@ -78,6 +128,9 @@ public class CorsResourceFilterTest extends JerseyTest {
         resourceConfig.register(MethodAnnotatedResource.class);
         resourceConfig.register(MethodAnnotatedResourceWithOverrides.class);
         resourceConfig.register(ClassAnnotatedResource.class);
+        resourceConfig.register(ClassAnnotatedWithOverridesResource.class);
+        resourceConfig.register(ClassAnnotatedWithOverridesAndAnnotateMethodsResource.class);
+        resourceConfig.register(ClassAnnotatedWithOverridesAndAnnotateMethodsWithOverridesResource.class);
 
 //        Map<String, Object> properties = new HashMap<>();
 //        properties.put(CorsConfig.ALLOW_ORIGIN, "http://foo.com");
@@ -87,56 +140,77 @@ public class CorsResourceFilterTest extends JerseyTest {
     }
 
     @Test
-    public void testUnAnnotated() {
-        Response response = doGet("unannotated");
+    public void testUnAnnotatedOptions() {
+        assertNoCorsHeadersResponseOptions(doOptions("unannotated"));
+    }
 
-        assertNoCorsHeadersResponse(response);
+    @Test
+    public void testUnAnnotated() {
+        assertNoCorsHeadersResponseGet(doGet("unannotated"));
     }
 
     @Test
     public void testCorsPreflight_MethodAnnotated() {
-        Response response = doPost("method-annotated");
-
-        assertDefaultOptionsResponse(response);
+        assertDefaultOptionsResponse(doOptions("method-annotated"));
     }
 
     @Test
     public void testCors_MethodAnnotated() {
-        Response response = doGet("method-annotated");
-
-        assertDefaultGetResponse(response);
+        assertDefaultGetResponse(doGet("method-annotated"));
     }
 
     @Test
     public void testCorsPreflight_MethodAnnotated_WithOverrides() {
-        Response response = doPost("method-annotated-overrides");
-
-        assertOverriddenOptionsResponse(response);
+        assertOverriddenOptionsResponse(doOptions("method-annotated-overrides"));
     }
 
     @Test
     public void testCors_MethodAnnotated_WithOverrides() {
-        Response response = doGet("method-annotated-overrides");
-
-        assertOverriddenGetResponse(response);
+        assertOverriddenGetResponse(doGet("method-annotated-overrides"));
     }
 
     @Test
     public void testCorsPreflight_ClassAnnotated() {
-        Response response = doPost("class-annotated");
-
-        assertDefaultOptionsResponse(response);
+        assertDefaultOptionsResponse(doOptions("class-annotated"));
     }
 
     @Test
     public void testCors_ClassAnnotated() {
-        Response response = doGet("class-annotated");
+        assertDefaultGetResponse(doGet("class-annotated"));
+    }
 
-        assertDefaultGetResponse(response);
+    @Test
+    public void testCorsPreflight_ClassAnnotated_WithOverrides() {
+        assertOverriddenOptionsResponse(doOptions("class-annotated-overrides"));
+    }
+
+    @Test
+    public void testCors_ClassAnnotated_WithOverrides() {
+        assertOverriddenGetResponse(doGet("class-annotated-overrides"));
+    }
+
+    @Test
+    public void testCorsPreflight_ClassAnnotated_WithOverrides_AndMethodAnnotations() {
+        assertOverriddenOptionsResponse(doOptions("class-annotated-overrides-annotated-methods"));
+    }
+
+    @Test
+    public void testCors_ClassAnnotated_WithOverrides_AndMethodAnnotations() {
+        assertOverriddenGetResponse(doGet("class-annotated-overrides-annotated-methods"));
+    }
+
+    @Test
+    public void testCorsPreflight_ClassAnnotated_WithOverrides_AndMethodAnnotations_WithOverrides() {
+        assertOverriddenOptionsResponse(doOptions("class-annotated-overrides-annotated-methods-overrides"));
+    }
+
+    @Test
+    public void testCors_ClassAnnotated_WithOverrides_AndMethodAnnotations_WithOverrides() {
+        assertOverriddenGetResponse(doGet("class-annotated-overrides-annotated-methods-overrides"));
     }
 
 
-    private Response doPost(String name) {
+    private Response doOptions(String name) {
         return target(name).request()
                 .header(ORIGIN, "foobar")
                 .options();
@@ -149,8 +223,19 @@ public class CorsResourceFilterTest extends JerseyTest {
     }
 
 
-    private static void assertNoCorsHeadersResponse(Response response) {
+    private static void assertNoCorsHeadersResponseGet(Response response) {
         Assert.assertEquals(200, response.getStatus());
+
+        assertFalse(response.getHeaders().containsKey(ALLOW_ORIGIN));
+        assertFalse(response.getHeaders().containsKey(ALLOW_CREDENTIALS));
+        assertFalse(response.getHeaders().containsKey(EXPOSE_HEADERS));
+        assertFalse(response.getHeaders().containsKey(MAX_AGE));
+        assertFalse(response.getHeaders().containsKey(ALLOW_METHODS));
+        assertFalse(response.getHeaders().containsKey(ALLOW_HEADERS));
+    }
+
+    private static void assertNoCorsHeadersResponseOptions(Response response) {
+        Assert.assertEquals(204, response.getStatus());
 
         assertFalse(response.getHeaders().containsKey(ALLOW_ORIGIN));
         assertFalse(response.getHeaders().containsKey(ALLOW_CREDENTIALS));
